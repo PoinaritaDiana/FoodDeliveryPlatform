b0abe2be4122987ee40d9ab6979357fbe0eb74a7
Diana Poinarita <61639111+PoinaritaDiana@users.noreply.github.com>
2021-04-02 19:06:30 +0200

    Add singleton class Cart

--- src/classes/Cart.java
+++ src/classes/Cart.java
@@ -1,4 +1,17 @@
 package classes;
 
-public class Cart {
+import java.util.HashSet;
+import java.util.Set;
+
+public final class Cart {
+    private static Cart userCart;
+    private Set<CartItem> cartProducts = new HashSet<>();
+
+    private Cart(){}
+
+    public static Cart getUserCart(){
+        if(userCart == null)
+            userCart = new Cart();
+        return userCart;
+    }
 }
