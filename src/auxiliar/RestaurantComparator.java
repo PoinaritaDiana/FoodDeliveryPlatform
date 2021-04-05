712bd2755aac8da5a2c9727e4f5b4c47f7c1b1bd
Diana Poinarita <61639111+PoinaritaDiana@users.noreply.github.com>
2021-04-05 17:14:55 +0200

    Add RestaurantComparator by rating for restaurantList

---
+++ src/auxiliar/RestaurantComparator.java
@@ -0,0 +1,12 @@
+package auxiliar;
+
+import classes.Restaurant;
+
+import java.util.Comparator;
+
+public class RestaurantComparator implements Comparator<Restaurant> {
+    @Override
+    public int compare(Restaurant r1, Restaurant r2){
+        return Float.compare(r1.getRating(),r2.getRating());
+    }
+}
