af7c81df776e950bfe0f488ef14502a7e7f1bcb2
Diana Poinarita <61639111+PoinaritaDiana@users.noreply.github.com>
2021-04-09 12:04:52 +0200

    Change calling the functionality corresponding to the user's option

--- src/services/Main.java
+++ src/services/Main.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
-
         // Get services
         Services services = Services.getServicesInstance();
 
